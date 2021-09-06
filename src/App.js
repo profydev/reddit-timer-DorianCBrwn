9c5dccfee77e0d4149faf20c511429748e54f7e4
Dorian Brown <brown.3794@gmail.com>
2021-09-06 18:41:42 -0400

    feat: :sparkles: Update import files
    add imports for react, react router and styled-components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { Normalize } from 'styled-normalize';
+import Header from './Header';
+import GlobalStyle from './GlobalStyle';
+import theme from './theme';
 
 function App() {
-  return (
-    <div>
-      App Placeholder
-    </div>
-  );
+  return <div>App Placeholder</div>;
 }
 
 export default App;
