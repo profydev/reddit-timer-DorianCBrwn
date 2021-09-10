3680e8b3963489534ed420470275cb940b586690
Dorian Brown <brown.3794@gmail.com>
2021-09-09 20:27:27 -0400

    update app skeleton

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { Normalize } from 'styled-normalize';
+import GlobalStyle from './GlobalStyles';
+import theme from './theme';
+
+function App() {
+  <ThemeProvider theme={theme}>
+    <Router>
+      <Normalize />
+      <GlobalStyle />
+      <Switch>
+        <Route exact path="/">
+          Home
+        </Route>
+        <Route path="/search">Search</Route>
+        <Route>404 - Not Found</Route>
+      </Switch>
+    </Router>
+  </ThemeProvider>;
+}
+
+export default App;
